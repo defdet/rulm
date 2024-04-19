da4e335e678f3a16fa086924411dbb6f1fed1afa
Max <bolgov1458@yandex.ru>
2024-04-19 20:43:34 +0300

    Update eval_rsg_tpu.py

--- self_instruct/src/benchmarks/eval_rsg_tpu.py
+++ self_instruct/src/benchmarks/eval_rsg_tpu.py
@@ -34,7 +34,7 @@ def load_easydel(path):
     sharding_axis_dims=(1, -1, 1, 1),
     sharding_axis_names=("dp", "fsdp", "tp", "sp"),
     backend="tpu",
-    input_shape=(1, 2048),
+    input_shape=(16, 2048),
     config_kwargs={
         "attn_mechanism": "normal",
     },
